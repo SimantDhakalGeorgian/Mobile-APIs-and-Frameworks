bc8b33514aba79646e30c42389063abcbefc3bca
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-10-18 18:54:20 -0400

    error while importing data
    error counts not defined

--- MidTerm/src/controllers/teamController.js
+++ MidTerm/src/controllers/teamController.js
@@ -5,9 +5,17 @@ const path = require('path');
 // Import teams from JSON file and show success and error based on the results
 const importTeams = async (req, res) => {
     try {
-        // get json data from file teams.json and store it in teamsData 
+        // get json data from file teams.json and store it in teamsData
         const teamsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/teams.json'), 'utf-8'));
-   
+        // add a count 
+        const count = await Team.countDocuments();
+
+        if (counts === 0) {
+            await Team.create(teamsData);
+            res.status(200).send('Teams successfully imported');
+        } else {
+            res.status(200).send('Teams already exist in the database');
+        }
         
     } catch (error) {
         console.error('Error importing data', error);
