10ae6d2e5a0ab8e5f06b84f3a16241d8d9691ca6
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-10-18 18:40:34 -0400

    fixed erro by adding config.env path

--- MidTerm/server.js
+++ MidTerm/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' }); // Load env variables
+
 
 
 const app = express();
