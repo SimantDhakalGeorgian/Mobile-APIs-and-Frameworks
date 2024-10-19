099677959c090cfeedfc8ab32d1c7b5250c5f8e0
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-10-18 20:20:20 -0400

    handle error in routes is incorrect
    show error message when routes is incorrect

--- MidTerm/server.js
+++ MidTerm/server.js
@@ -28,8 +28,20 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
+// 404 error handling middleware
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found: The requested resource could not be found. Please check the routes and try again');
+});
+
+// Error handling middleware for server errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong. Try again later.');
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
