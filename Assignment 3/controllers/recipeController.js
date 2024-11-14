572fc9aaa0f2d6b5a672af952de1c569f7f03646
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-11-13 19:56:57 -0500

    added an api to get a single recipe by ID

--- Assignment 3/controllers/recipeController.js
+++ Assignment 3/controllers/recipeController.js
@@ -24,4 +24,18 @@ router.get('/', verifyToken, async (req, res) => {
   }
 });
 
+//Get a single recipe by Id
+router.get('/:id', verifyToken, async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({ message: 'Recipe not found' });
+    }
+    res.status(200).json(recipe);
+  } catch (error) {
+    console.error('Error retrieving the recipe:', error);
+    res.status(500).json({ message: 'Error retrieving the recipe' });
+  }
+});
+
 module.exports = router;
