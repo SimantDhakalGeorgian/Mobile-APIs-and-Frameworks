0349f69a3ecfc1afa42fb9a787be6be9e89dfeb3
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-12-06 02:12:13 -0500

    Create user model

---
+++ Assignment 4 - Secure API with full CRUD UI/models/user.js
@@ -0,0 +1,28 @@
+/**
+ * File name: user.js
+ * Student Name: Simant Dhakal
+ * StudentID: 200563270
+ * Date: December 6, 2024
+ */
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  fullname: { type: String, required: true},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Create and export the model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
