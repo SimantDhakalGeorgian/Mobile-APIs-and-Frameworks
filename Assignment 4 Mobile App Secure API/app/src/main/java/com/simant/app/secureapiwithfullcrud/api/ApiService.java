b2214bf0a32355b61fa4f6149cdcbad55e8ace3c
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-12-06 05:53:26 -0500

    Create api service for end point

---
+++ Assignment 4 Mobile App Secure API/app/src/main/java/com/simant/app/secureapiwithfullcrud/api/ApiService.java
@@ -0,0 +1,21 @@
+package com.simant.app.secureapiwithfullcrud.api;
+
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+
+public interface ApiService {
+
+
+}
