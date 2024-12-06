e27caeda8b81216c882ea3652a5c8f94bb85c2c2
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-12-06 06:06:18 -0500

    added user model

--- Assignment 4 Mobile App Secure API/app/src/main/java/com/simant/app/secureapiwithfullcrud/RegisterActivity.java
+++ Assignment 4 Mobile App Secure API/app/src/main/java/com/simant/app/secureapiwithfullcrud/RegisterActivity.java
@@ -1,24 +1,49 @@
 package com.simant.app.secureapiwithfullcrud;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText etFullName, etEmail, etPassword;
+    private Button btnRegister;
+    private RegisterController registerController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etFullName = findViewById(R.id.etFullName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        registerController = new RegisterController();
+
+        btnRegister.setOnClickListener(view -> {
+            String fullname = etFullName.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (fullname.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+            } else {
+                User user = new User(fullname, email, password);
+                registerController.registerUser(user, new RegisterController.RegisterCallback() {
+                    @Override
+                    public void onSuccess(String message) {
+                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Toast.makeText(RegisterActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         });
     }
 }
