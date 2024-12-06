49939d6bb5c741dc84a15cca11aeecbdf86d6880
SimantDhakalGeorgian <200563270@student.georgianc.on.ca>
2024-12-06 07:48:24 -0500

    recipe model is added to parsa all rcipe from api

---
+++ Assignment 4 Mobile App Secure API/app/src/main/java/com/simant/app/secureapiwithfullcrud/models/Recipe.java
@@ -0,0 +1,89 @@
+package com.simant.app.secureapiwithfullcrud.models;
+
+import java.util.List;
+
+public class Recipe {
+    private String _id;
+    private String recipeName;
+    private List<String> ingredients;
+    private int cookingTime;
+    private String difficulty;
+    private String cuisine;
+    private String description;
+    private String photoLink;
+    private double averageRating;
+
+    // Getters and Setters
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(int cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+}
+
